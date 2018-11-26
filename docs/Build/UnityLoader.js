ce5b11d129c7669faa5272cc4c52eb6856aed6b0
hamidhomatash <hamidhomatash@hotmail.com>
2018-11-26 14:21:36 +0000

    Disabling first error in IE

--- docs/Build/UnityLoader.js
+++ docs/Build/UnityLoader.js
@@ -3,7 +3,7 @@ var UnityLoader = UnityLoader || {
 		UnityLoader.SystemInfo.hasWebGL ? UnityLoader.SystemInfo.mobile ? e.popup("Please note that Unity WebGL is not currently supported on mobiles. Press OK if you wish to continue anyway.", [{
 			text: "OK",
 			callback: t
-		}]) : ["Firefox", "Chrome", "Safari"].indexOf(UnityLoader.SystemInfo.browser) == -1 ? e.popup(UnityLoader.SystemInfo.browser + " Please note that your browser is not currently supported for this Unity WebGL content. Press OK if you wish to continue anyway.", [{
+		}]) : ["Firefox", "Chrome", "Safari", "Microsoft Internet Explorer"].indexOf(UnityLoader.SystemInfo.browser) == -1 ? e.popup("Please note that your browser is not currently supported for this Unity WebGL content. Press OK if you wish to continue anyway.", [{
 			text: "OK",
 			callback: t
 		}]) : t() : e.popup("Your browser does not support WebGL", [{
